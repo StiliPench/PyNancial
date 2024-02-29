cd9696e4e7d77b38a26990765b8178d2dc89e342
StiliPench <stilipench@gmail.com>
2024-02-29 17:46:18 +0100

    Calculating undervalue index and getting top stocks

--- backend/services.py
+++ backend/services.py
@@ -4,7 +4,7 @@ import csv
 
 metrics = ['trailingPE', 'forwardPE', 'priceToBook', 'debtToEquity', 'dividendYield', 'freeCashflow']
 # Metrics for which lower is better
-invert_list = ['trailingPE', 'forwardPE', 'priceToBook', 'debtToEquity']
+invert_list = set(['trailingPE', 'forwardPE', 'priceToBook', 'debtToEquity'])
 weights = {'trailingPE': 0.18, 'forwardPE': 0.12, 'priceToBook': 0.25, 'debtToEquity': 0.2, 'dividendYield': 0.15, 'freeCashflow': 0.1}
 
 
@@ -25,7 +25,6 @@ def fetch_stocks(stock_symbols):
 
     return stock_data
 
-
 def filter_stocks(stock_data, exchange='Any', sector='Any', min_market_cap=0):
     filtered_stocks = []
 
@@ -34,49 +33,66 @@ def filter_stocks(stock_data, exchange='Any', sector='Any', min_market_cap=0):
         # Early skip conditions that don't meet the criteria
         if (sector != 'Any' and stock_info.get('sector') != sector) or \
            (exchange != 'Any' and stock_info.get('exchange') != exchange) or \
-           (stock_info.get('marketCap') < min_market_cap):
+           (stock_info.get('marketCap', 0) < min_market_cap):
             continue
         # If all conditions are met append the data directly
         filtered_stocks.append(stock_info)
     return filtered_stocks
 
 # Gets min and max value for each metric, needed for normalization
-def get_min_max_values(stock_data, metrics):
+def get_min_max_values(stock_data):
 
     min_max_values = {metric: {'min': float('inf'), 'max': float('-inf')} for metric in metrics}
 
     for stock in stock_data:
         for metric in metrics:
-            value = stock.get(metric, 0)
-            if value is not None:
+            value = stock.get(metric)
+            if isinstance(value, float) or isinstance(value, int):
                 min_max_values[metric]['min'] = min(min_max_values[metric]['min'], value)
                 min_max_values[metric]['max'] = max(min_max_values[metric]['max'], value)
     
     return min_max_values
 
 def normalize_value(value, min_value, max_value, invert=False):
-    value = 0 if value is None else value
     if max_value - min_value == 0:
-        return 0
+        return 1 if invert else 0
     normalized = (value - min_value) / (max_value - min_value)
     return 1 - normalized if invert else normalized
 
-def calculate_undervalue_index(stock, min_max_values, weights, invert_list):
+def calculate_undervalue_index(stock, min_max_values):
     undervalue_index = 0
     for metric, weight in weights.items():
 
         min_value, max_value = min_max_values[metric]['min'], min_max_values[metric]['max']
         metric_value = stock.get(metric)
-        invert = metric in invert_list
-        normalized_value = normalize_value(metric_value, min_value, max_value, invert=invert)
-        undervalue_index += normalized_value * weight
+
+        if isinstance(metric_value, float) or isinstance(metric_value, int):
+            invert = metric in invert_list
+            normalized_value = normalize_value(metric_value, min_value, max_value, invert=invert)
+            undervalue_index += normalized_value * weight
 
     return undervalue_index
 
-stock_symbols = get_stock_symbols('stock_symbols_short.csv')
+def get_top_stocks(stock_data, n):
+    # Calculate undervalue index for each stock
+    min_max_values = get_min_max_values(stock_data)
+    for stock in stock_data:
+        stock['undervalue_index'] = calculate_undervalue_index(stock, min_max_values)
+
+    # Sort stocks by undervalue index in descending order
+    sorted_stocks = sorted(stock_data, key=lambda x: x['undervalue_index'], reverse=True)
+
+    # Return the top n stocks
+    return sorted_stocks[:n]
+
+stock_symbols = get_stock_symbols('symbol_list_final.csv')
 stock_data = fetch_stocks(stock_symbols)
 filtered_stock_data = filter_stocks(stock_data)
-min_max_values = get_min_max_values(stock_data, metrics)
+top_stocks = get_top_stocks(filtered_stock_data, 120)
 
-for stock in filtered_stock_data:
-    print(stock.get('symbol'),calculate_undervalue_index(stock, min_max_values, weights, invert_list))
+#for stock in filtered_stock_data:
+ #   for metric in metrics:
+  #      print(stock.get('symbol'), type(stock.get(metric)))
+
+for stock in top_stocks:
+    print(f"Symbol: {stock['symbol']}, Undervalue Index: {stock['undervalue_index']}")
